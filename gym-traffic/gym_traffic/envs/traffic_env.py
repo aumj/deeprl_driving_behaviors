a7ab609793aa50a217689226c5f2cd679614a744
Aum Jadhav <aum.jadhav@gmail.com>
2017-12-03 17:06:40 -0500

    Updated reward function to include collisions

--- gym-traffic/gym_traffic/envs/traffic_env.py
+++ gym-traffic/gym_traffic/envs/traffic_env.py
@@ -111,22 +111,25 @@ class TrafficEnv(Env):
         min_dist = 100.00
 
     	ego_pos = np.array(traci.vehicle.getPosition(self.ego_veh_vehID))
-    	for i in traci.vehicle.getIDList(): 
+    	for i in traci.vehicle.getIDList():
     		# excluding ego vehicle AND any vehicle from the opposite direction (NS) for comparison
     		if i != self.ego_veh_vehID and i.find('flow_n_s') == -1:
     			pos = np.array(traci.vehicle.getPosition(i))
     			new_dist = np.linalg.norm(ego_pos - pos)
-    			if new_dist < min_dist: 
+    			if new_dist < min_dist:
     				min_dist = new_dist
 
-    	# mark "warning" as True when cars get too close OR the actual collision takes place 
+    	# mark "warning" as True when cars get too close OR the actual collision takes place
     	if min_dist < 3.0: warning = True
         return (warning, min_dist)
 
     # TO DO: re-define reward function!!
     def _reward(self):
+        (warning, min_dist) = self.check_collision()
         if traci.vehicle.getPosition(self.ego_veh_vehID)[1] >= self.ego_veh_goal_pos:
             reward = 1000
+        elif warning:
+            reward = -5000
         else:
             reward = -0.1
         return reward
@@ -144,12 +147,12 @@ class TrafficEnv(Env):
         traci.simulationStep()
         observation = self._observation()
         reward = self._reward()
-        
+
         print self.check_collision()
 
         done = (traci.vehicle.getPosition(self.ego_veh_vehID)[1] >= self.ego_veh_goal_pos) \
                or (self.sumo_step > self.simulation_end) \
-               or (self.ego_veh_vehID not in traci.vehicle.getIDList()) 
+               or (self.ego_veh_vehID not in traci.vehicle.getIDList())
         # self.screenshot()
         if done:
             self.stop_sumo()
